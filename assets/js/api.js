81ddf3bf8d43a8dccb96b4b477e0cb037d630038
arifkarianzah <arifkarianzah@gmail.com>
2026-08-04 00:52:22 +0700

    feat(frontend): connect API to Azure App Service domain

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,8 +1,8 @@
 // assets/js/api.js
-// Otomatis gunakan URL backend (Vercel Serverless atau Localhost)
+// Otomatis gunakan URL backend Azure App Service atau Localhost
 const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:3000/api'
-  : '/api';
+  : 'https://mentangor-rg-ejftf2a4cxd8hhgj.southeastasia-01.azurewebsites.net/api';
 
 const API = {
   getHeaders: () => {
